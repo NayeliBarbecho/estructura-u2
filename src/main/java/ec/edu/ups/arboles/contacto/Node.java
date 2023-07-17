6cdd284fecb5503240ec63ee5d8b43850468b349
NayeliBarbecho <nayelicajamarca.98@gmail.com>
2023-07-17 09:46:15 -0500

    first commit

---
+++ src/main/java/ec/edu/ups/arboles/contacto/Node.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.ups.arboles.contacto;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Node {
+    private Contacto contacto;
+    private Node left;
+    private Node right;
+
+    public Node(Contacto contacto) {
+        this.contacto = contacto;
+        this.left = null;
+        this.right = null;
+    }
+
+    public Contacto getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(Contacto contacto) {
+        this.contacto = contacto;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" + "contacto=" + contacto + ", left=" + left + ", right=" + right + '}';
+    }
+    
+}
